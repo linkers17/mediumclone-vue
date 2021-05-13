ed15d423d1048be20f9628e3d246fc81b69e5a16
linkers17 <romeo-gry@mail.ru>
2021-05-13 15:41:04 +0700

    Добавил разметку в фид

--- src/components/FeedComponent.vue
+++ src/components/FeedComponent.vue
@@ -1,9 +1,46 @@
 <template>
-    <div>Feed</div>
+    <div>
+        <div v-if="isLoading">Loading...</div>
+        <div v-if="error">Something bad happened</div>
+        <div v-if="feed">
+            <div class="article-preview" v-for="(article, index) in feed.articles" :key="index">
+                <div class="article-meta">
+                    <router-link
+                        :to="{name: 'userProfile', params: {slug: article.author.username}}"
+                    >
+                        <img :src="article.author.image" alt="avatar">
+                    </router-link>
+                    <div class="info">
+                        <router-link
+                            class="author"
+                            :to="{name: 'userProfile', params: {slug: article.author.username}}"
+                        >
+                            {{ article.author.username }}
+                        </router-link>
+                        <span class="date">{{ article.createdAt }}</span>
+                    </div>
+                    <div class="pull-xs-right">
+                        ADD TO FAVORITES
+                    </div>
+                </div>
+                <router-link
+                    :to="{name: 'article', params: {slug: article.slug}}"
+                    class="preview-link"
+                >
+                    <h2>{{ article.title }}</h2>
+                    <p>{{ article.description }}</p>
+                    <span>Read more...</span>
+                    TAG LIST
+                </router-link>
+            </div>
+            PAGINATION
+        </div>
+    </div>
 </template>
 
 <script>
     import { actionTypes } from "@/store/modules/feed";
+    import {mapState} from "vuex";
 
     export default {
         name: 'McvFeed',
@@ -12,6 +49,13 @@
                 type: String,
                 required: true
             }
+        },
+        computed: {
+            ...mapState({
+                isLoading: state => state.feed.isLoading,
+                feed: state => state.feed.data,
+                error: state => state.feed.error
+            })
         },
         mounted() {
             this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl});
--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,6 @@ import GlobalFeedComponent from "@/views/GlobalFeedComponent";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: GlobalFeedComponent
-  },
-  {
     path: '/login',
     name: 'login',
     component: LoginComponent
@@ -18,7 +13,52 @@ const routes = [
     path: '/register',
     name: 'register',
     component: RegisterComponent
-  }
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: GlobalFeedComponent
+  },
+  {
+    path: '/feed',
+    name: 'yourFeed',
+    component: GlobalFeedComponent
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: GlobalFeedComponent
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: GlobalFeedComponent
+  },
+  {
+    path: '/articles/:slug/edit',
+    name: 'editArticle',
+    component: GlobalFeedComponent
+  },
+  {
+    path: '/articles/:slug',
+    name: 'article',
+    component: GlobalFeedComponent
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: GlobalFeedComponent
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: GlobalFeedComponent
+  },
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: GlobalFeedComponent
+  },
 ]
 
 const router = createRouter({
